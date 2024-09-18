4982c82319d0d3bed5bf2366f67736565070f1d2
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-09-18 11:52:00 -0400

    added more pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,12 @@
                         <NuxtLink to="/home">Home</NuxtLink>
                     </li>
                     <li>
+                        <NuxtLink to="/lakes">The Great Lakes</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/activities">Things To Do</NuxtLink>
+                    </li>
+                    <li>
                         <NuxtLink to="/gallery">Gallery</NuxtLink>
                     </li>
                 </ul>
