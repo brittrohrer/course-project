d1b1b20a9847e83b786b6ac2749592603c16837d
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-09-18 11:37:58 -0400

    first layout and page

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/gallery">Gallery</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <slot />
+    </div>
+</template>
