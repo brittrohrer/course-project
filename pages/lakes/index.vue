3966316411ddbbfefe75fc4d26f3ad1aa8c5669b
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-09-19 12:34:04 -0400

    added lake pics to accordion

--- pages/lakes/index.vue
+++ pages/lakes/index.vue
@@ -11,7 +11,8 @@ const accordion = ref([
         body: `The Ojibwe name is gitchi-gami, which translates to “Great Sea”. Lake Superior is the largest freshwater lake in the world by surface area.  By volume, it is the largest of the Great Lakes. 
                 Facts:
                 Max Depth: 1333 ft
-                Surrounded By: Michigan, Wisconsin, Minnesota, and Ontario`
+                Surrounded By: Michigan, Wisconsin, Minnesota, and Ontario`,
+        img: `/lakesu.jpg`
     },
     {
         header: `Lake Michigan`,
@@ -21,7 +22,8 @@ const accordion = ref([
         body: `From the Ojibwe name, mishi-gami, which translate to “Great Water”. Lake Michigan is the second-largest lake, out of the Great Lakes, by volume. It is connected to Lake Huron through the Straits of Mackinac. The Shores of Lake Michigan is often referred to as the “Third Coast” following those of the Atlantic and Pacific Oceans.
                 Facts:
                 Max Depth: 925 ft
-                Surrounded By: Michigan, Wisconsin, Illinois, and Indiana`
+                Surrounded By: Michigan, Wisconsin, Illinois, and Indiana`,
+        img: `/lakemi.jpeg`
     },
     {
         header: `Lake Huron`,
@@ -31,7 +33,8 @@ const accordion = ref([
         body: `It is connected to Lake Michigan through the Straits of Mackinac. It is the third-largest lake, by volume, of the five Great Lakes. 
                 Facts:
                 Max Depth: 748 ft
-                Surrounded By: Michigan and Ontario`
+                Surrounded By: Michigan and Ontario`,
+        img: `/lakehur.jpg`
     },
     {
         header: `Lake Ontario`,
@@ -41,7 +44,8 @@ const accordion = ref([
         body: `Lake Ontario is the fourth-largest lake of the five, by volume. It is also the only lake to not border the state of Michigan. This is the last lake in the chain of Great Lakes that leads to the St. Lawerence River, which ends at the Atlantic Ocean.
                 Facts:
                 Max Depth: 804 ft
-                Surrounded By: Ontario and New York`
+                Surrounded By: Ontario and New York`,
+        img: `/lakeo.jpg`
     },
     {
         header: `Lake Erie`,
@@ -51,7 +55,8 @@ const accordion = ref([
         body: `It’s name is derived from the Iroquoian term “erielhonan” which translates to “long tail”Lake Erie is the smallest, by volume, of all the Great Lakes. It flows into Lake Ontario through the Niagara Falls.
                 Facts:
                 Max Depth: 210 ft
-                Surrounded By: Michigan, Ohio, Pennsylvania, New York, and Ontario`
+                Surrounded By: Michigan, Ohio, Pennsylvania, New York, and Ontario`,
+        img: `/lakeer.jpg`
     },
 ],); 
 const show = ref(true)
@@ -81,7 +86,8 @@ const show = ref(true)
                 <!-- identify target based on id and show the body -->
                 <div :id="a.id" class="accordion-collapse collapse" data-bs-parent="#myFirstAccordion">
                     <div v-show="show" class="accordion-body">
-                        {{a.body}}
+                        <p>{{a.body}}</p>
+                        <img :src="a.img">
                     </div>
                 </div>
             </div>
