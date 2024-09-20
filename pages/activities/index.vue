ee148d4d69d4df2f8e77f36ea4e5cd38847bc19f
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-09-19 23:29:29 -0400

    added to grid and styled

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -1,50 +1,45 @@
 <script setup>
 import { ref } from "vue";
 /* Create array and header variable to dynamically build page content for each grid container */
-const headerOne = ref("Natural Wonders")
+const headerOne = ref("National Park Destinations:")
 const gridOne = ref([
     {
-        class: `col-lg-12`,
-        title: `Title`,
-        body: `There are six locations in Michigan that are managed by the National Park Service.`
-    },
-    {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `Isle Royale National Park`,
         body: `Isle Royale National Park is an island located in the northern region of Lake Superior. 
                 Isle Royale encompasses an area of 132,018 acres, and can only be explored by foot. The park can only be reached by ferry or seaplane and only from April to November.`
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `Keweenaw National Historical Park`,
         body: `This park consists of 21 historic sites along with the old copper mines. It is home to the world’s oldest and largest lava flow, which was first mined more than 7,000 years ago.`               
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `North Country National Scenic Trail`,
         body: `The North Country Trail is a 4,800 mile trail that goes from Vermont to North Dakota. It is the longest of the National Scenic Trails. The Upper Peninsula includes 550 miles of the 
                 trail while the Lower Peninsula includes 600 miles. `
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `Pictured Rocks National Lakeshore`,
         body: `The park includes miles of lakeshore with sandstone cliffs, beaches, waterfalls and sand dunes. It is one of the most popular and visited locations in Michigan. The minerals seeping 
                 from the cliff side create an almost painted scene down the cliff to Lake Superior. `               
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `River Raisin National Battlefield Park`,
         body: `This is the historic location of the River Raisin Battle that took place in January 1813 during the War of 1812. The River Raisin Battle resulted in the forced relocation and assimilation 
                 of the Native Peoples who resided there.`
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4 col-md-6`,
         title: `Sleeping Bear Dunes National Lakeshore`,
         body: `This National Lakeshore includes the world’s largest collection of sand dunes along fresh water. They include 450 ft tall sand dunes. The lakeshore consists of 35 miles along Lake Michigan and 
                 some of the prettiest beaches in the Midwest.`               
     },
 ],);
-const headerTwo = ref("Other Destinations")
+const headerTwo = ref("Other Top Destinations:")
 const gridTwo = ref([
     {
         class: `col-lg-4`,
@@ -56,14 +51,14 @@ const gridTwo = ref([
                 Edmund Fitzgerald. `
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4`,
         title: `Tahquamenon Falls`,
         body: `The Falls consist of the Lower and Upper Falls. The Upper Falls are one of the largest waterfalls east of the Mississippi 
                 River. Its spans 200 ft wide with a nearly 50 ft drop. Tannins released from the cedar and hemlock swamps, located upriver, 
                 give the falls its signature brown color and white foam.`
     },
     {
-        class: `col-lg-4 text-center`,
+        class: `col-lg-4`,
         title: `Mackinac Island`,
         body: `An island located in Lake Huron near the Straights of Mackinac. The island is listed as a National Historic Landmark and a majority 
                 of the island is preserved as Mackinac Island State Park. Mackinac Island is well known for its ban on almost all motor vehicles and 
@@ -71,54 +66,88 @@ const gridTwo = ref([
                 of America.`               
     },
 ],);
-const headerThree = ref("Title")
+const headerThree = ref("Popular Events:")
 const gridThree = ref([
     {
-        class: `col-sm-6`,
-        title: `title`,
-        body: `body` 
+        class: `col-lg-6`,
+        title: `Michigan Renaissance Festival`,
+        body: `Interactive Event that takes place annually August to October in Holly, Michigan. 
+                There is music, local artisans, and even jousting.`
     },
     {
-        class: `col-sm-6`,
-        title: `title`,
-        body: `body`
+        class: `col-lg-6`,
+        title: `Tulip Time Festival`,
+        body: `Takes place in Holland, Michigan in the beginning of May. It is the longest running tulip festival in the United States. 
+                Activities include parades, fireworks, music and more. The festival also features millions of tulips.`
     },
+    {
+        class: `col-lg-6`,
+        title: `National Cherry Festival`,
+        body: `Takes place in Traverse City, Michigan in August. It is a food festival named after the region, which is nicknamed the 
+                "Cherry Capital of the World". Includes great food, family activities and live-entertainment.`
+    },
+    {
+        class: `col-lg-6`,
+        title: `National Blueberry Festival`,
+        body: `Every year in August, South Haven Michigan hosts the National Blueberry Festival.
+             There is live music, parades, classic car shows, and blueberry-themed competitions.`
+    },
+    {
+        class: `col-lg-6`,
+        title: `Bavarian Festival`,
+        body: `Held in Frankenmuth, Michigan. Sing and dance to Bavarian music. Also includes German entertainers and polka bands`
+    },
+    {
+        class: `col-lg-6`,
+        title: `Electric Forest Music Festival`,
+        body: `The festival is a multiday music festival that features a variety of bands including electronic dance music.`
+    },
+    {
+        class: `col-lg-6`,
+        title: `Faster Horses Music Festival`,
+        body: `Multiday country music festival held in Brooklyn, Michigan.`
+    }
 ],);
+
 </script>
 
 <template>
-    <main class="container">
+    <main class="container mt-5">
+        <h2>Popular Destinations and Events</h2>
+        <div class="container">
         <!-- use a v-for for each grid section to display the header, title and body with each of their specified layouts-->
-        <div class="row bg-primary">
-            <h2>{{headerOne}}</h2>
-            <div :class="gOne.class"
-            v-for="gOne in gridOne"
-            :key="gOne.title">
-                <h3 class="text-center">{{gOne.title}}</h3>
-                <p>{{gOne.body}}</p>
+            <div class="grid-container row">
+                <h3 class="grid-header">{{headerOne}}</h3>
+                <div :class="gOne.class"
+                v-for="gOne in gridOne"
+                :key="gOne.title">
+                    <h4 class="text-center">{{gOne.title}}</h4>
+                    <p>{{gOne.body}}</p>
+                </div>
             </div>
-        </div>
         
-        <div class="row bg-info">
-            <h2>{{headerTwo}}</h2>
-            <div :class="gTwo.class"
-                v-for="gTwo in gridTwo"
-                :key="gTwo.title">
-                <h3 class="text-center">{{gTwo.title}}</h3>
-                <p>{{gTwo.body}}</p>
+            <div class="grid-container row">
+                <h3 class="grid-header">{{headerTwo}}</h3>
+                <div :class="gTwo.class"
+                    v-for="gTwo in gridTwo"
+                    :key="gTwo.title">
+                    <h4 class="text-center">{{gTwo.title}}</h4>
+                    <p>{{gTwo.body}}</p>
+                </div>
             </div>
-        </div>
         
-        <div class="row bg-warning">
-            <h2>{{headerThree}}</h2>
-            <div :class="gThree.class"
-                v-for="gThree in gridThree"
-                :key="gThree.title">
-                <h3 class="text-center">{{gThree.title}}</h3>
-                <p>{{gThree.body}}</p>
+            <div class="grid-container row">
+                <h3 class="grid-header">{{headerThree}}</h3>
+                <div :class="gThree.class"
+                    v-for="gThree in gridThree"
+                    :key="gThree.title">
+                    <h4 class="text-center">{{gThree.title}}</h4>
+                    <p>{{gThree.body}}</p>
+                </div>
             </div>
         </div>
     </main>
+    
 </template>
 
 <style scoped>
