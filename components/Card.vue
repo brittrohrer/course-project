032ccbee8d436bd87b6d179e3e267f578ace54a0
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-09-23 22:54:28 -0400

    added color statements

--- components/Card.vue
+++ components/Card.vue
@@ -23,8 +23,26 @@ function displayColorTemp(e) {
     if(on === true) {
         console.log('got this ' + on);
         console.log(temps);
+        if (temps > 90) {
+            e.target.style.backgroundColor = "#E66B39";
+            e.target.style.border = "none";
+        } else if (temps < 90 && temps > 80) {
+            e.target.style.backgroundColor = "#F4B96D";
+            e.target.style.border = "none";
+        } else if (temps < 80 && temps > 70) {
+            e.target.style.backgroundColor = "#F5EFB2";
+            e.target.style.border = "none";
+        } else if (temps < 70 && temps > 60) {
+            e.target.style.backgroundColor = "#74B985";
+            e.target.style.border = "none";
+        } else {
+            e.target.style.backgroundColor = "#26798A";
+            e.target.style.border = "none";
+        }
     } else {
         console.log('not today' + on);
+        e.target.style.backgroundColor = "white";
+        e.target.style.border = "solid gray 1px";
     }
 }
 
