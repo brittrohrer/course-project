448732c3c407c2122126213da62b64caebc55679
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-10-03 16:53:14 -0400

    changed nav

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,30 +1,11 @@
 <script setup>
 import {ref} from "vue";
 
-/* use variable that can be changed and create web content dynamically */
-const title =ref(`Michigan`);
 
 </script>
 <template>
     <header>
         <div class="banner"> 
-            <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-                <h1 class="nav-title navbar-brand">{{ title }}</h1>
-                <ul class="nav navbar-nav">
-                    <li class="nav-item">
-                        <NuxtLink to="/">Home</NuxtLink>
-                    </li>
-                    <li class="nav-item">
-                        <NuxtLink to="/lakes">The Great Lakes</NuxtLink>
-                    </li>
-                    <li class="nav-item">
-                        <NuxtLink to="/activities">Things To Do</NuxtLink>
-                    </li>
-                    <li class="nav-item">
-                        <NuxtLink to="/gallery">Gallery</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
         </div>     
     </header>
 </template>
